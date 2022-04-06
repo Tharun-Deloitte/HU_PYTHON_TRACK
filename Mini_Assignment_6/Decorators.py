7402de81a37718253776fc346bd1cc2bd059b89b
Reddy <naratreddy@deloitte.com>
2022-04-06 17:04:45 +0530

    Mini_Assignment_6

---
+++ Mini_Assignment_6/Decorators.py
@@ -0,0 +1,16 @@
+def smart(func):
+    def inner(num1, num2):
+        func(num1, num2)
+        func(num1, num2)
+
+    return inner
+
+@smart
+def multiply(n1, n2):
+    print(n1 * n2)
+
+
+n1 = int(input('Enter first number: '))
+n2 = int(input('Enter second number: '))
+
+multiply(n1, n2)
